15da94f531b582b5edc5561ed7561a81b3387a2a
Pat McClernan <patmccler@gmail.com>
2020-05-07 17:26:53 -0400

    builds transfer and bank_account methods

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,36 @@
 class BankAccount
+  attr_reader :name
+  attr_accessor :balance, :status
+  @all = []
 
+  def initialize name
+    @status = "open"
+    @name = name
+    @balance = 1000.00
+  end
+
+  def self.all
+    @all
+  end
+
+  def self.find_account name
+    all.find { |account| account.name == name }
+  end
+
+  def deposit(amount)
+    @balance += amount
+  end
+
+  def display_balance
+    "Your balance is $#{@balance}."
+  end
+
+  def valid?
+    @status == "open" && balance.positive?
+  end
+
+  def close_account
+    @status = "closed"
+    @balance = 0
+  end
 end
